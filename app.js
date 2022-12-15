aec2bbe0223828e8e7303bcbea1575b9ac6cdae8
Aidanc2000 <aidan@carvalho.org.uk>
2022-12-15 13:51:12 +0000

    updated paths, signup is now first page, successful login now goes to /welcome -  separate from previous /log-in

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const { SALT, SESSION_SECRET, SERVER_PORT } = process.env;
-const options = { verbose: console.log };
+const {SALT, SESSION_SECRET, SERVER_PORT} = process.env;
 const Database = require('better-sqlite3');
 
 // ----------------------------------------------------------------
 // connect to db
-const db = new Database('users.db', { verbose: console.log });
+const db = new Database('users.db', {verbose: console.log});
 db.exec(`CREATE TABLE IF NOT EXISTS users (username, password)`)
 // ----------------------------------------------------------------
 
@@ -19,130 +18,134 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/styles'));
 app.use(
-	session({
-		secret: SESSION_SECRET,
-		resave: false,
-		saveUninitialized: true,
-	})
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
 );
 
 app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(passport.initialize());
 
+app.get('/log-in', (req, res) => {
+    let error = null;
+    if (req.session && req.session.messages && req.session.messages.length > 0) {
+        error = {message: req.session.messages[0]};
+    }
+    res.render('index', {
+        user: req.user,
+        error: error,
+        signUpSuccessful: false,
+    });
+});
+
 app.get('/', (req, res) => {
-	let error = null;
-
-	if (req.session && req.session.messages && req.session.messages.length > 0) {
-		error = { message: req.session.messages[0] };
-	}
-	res.render('index', {
-		user: req.user,
-		error: error,
-		signUpSuccessful: false,
-	});
+    res.render('sign-up-form', {error: null});
 });
 
-app.get('/sign-up', (req, res) => {
-	res.render('sign-up-form', { error: null });
+app.get('/welcome', (req, res) => {
+    res.render('welcome', {user: req.user, error: null});
 });
 
-app.post('/sign-up', async (req, res) => {
-	try {
-		let username = req.body.username;
-		let password = req.body.password;
-		if (username.length < 6) {
-			throw new Error('Password Must Be At Least 6 characters');
-		}
-		if (password.length > 16) {
-			throw new Error('Password Must Be Less Than 16 characters');
-		}
-		if (password == 'password') {
-			throw new Error('Password cannot be password');
-		}
+app.post('/', async (req, res) => {
+    try {
+        let username = req.body.username;
+        let password = req.body.password;
+        if (username.length < 6) {
+            throw new Error('Password Must Be At Least 6 characters');
+        }
+        if (password.length > 16) {
+            throw new Error('Password Must Be Less Than 16 characters');
+        }
+        if (password == 'password') {
+            throw new Error('Password cannot be password');
+        }
 
-		const user = db
-			.prepare('SELECT * FROM users WHERE username = ?')
-			.get(username);
+        const user = db
+            .prepare('SELECT * FROM users WHERE username = ?')
+            .get(username);
 
-		if (user) {
-			throw new Error('Username In Use');
-		}
+        if (user) {
+            throw new Error('Username In Use');
+        }
 
-		const results = db
-			.prepare(
-				`INSERT INTO users (username, password) VALUES (@username,@password)`
-			)
-			.run({ username, password });
+        const results = db
+            .prepare(
+                `INSERT INTO users (username, password) VALUES (@username,@password)`
+            )
+            .run({username, password});
 
-		const newUser = db
-			.prepare('SELECT * FROM users WHERE username = ?')
-			.get(username);
-		if (newUser) {
-			res.render('index', { user: null, signUpSuccessful: true , error: null});
-		}
-		// const saltedPassword = await hashedPassword(req.body.password);
-	} catch (error) {
-		res.status(400).render('sign-up-form', { error: error });
-	}
+        const newUser = db
+            .prepare('SELECT * FROM users WHERE username = ?')
+            .get(username);
+        if (newUser) {
+            res.render('index', {user: null, signUpSuccessful: true, error: null});
+        }
+        // const saltedPassword = await hashedPassword(req.body.password);
+    } catch (error) {
+        res.status(400).render('sign-up-form', {error: error});
+    }
 });
 
 app.post(
-	'/log-in',
-	passport.authenticate('local', {
-		successRedirect: '/',
-		failureRedirect: '/',
-		failureMessage: true,
-	})
+    '/log-in',
+    passport.authenticate('local', {
+        successRedirect: '/welcome',
+        failureRedirect: '/log-in',
+        failureMessage: true,
+    })
 );
 
+
 app.get('/log-out', (req, res, next) => {
-	req.logout(function (err) {
-		if (err) {
-			return next(err);
-		}
-		res.redirect('/');
-	});
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
 });
 // ----------------------------------------------------------------
 
 passport.use(
-	new LocalStrategy(
-		{ passReqToCallback: true },
-		async (req, username, password, done) => {
-			req.session.messages = [];
-			try {
-				const user =  db
-					.prepare('SELECT * FROM users WHERE username = ?')
-					.get(username);
-				if (!user) {
-					return done(null, false, { message: 'Incorrect Username' });
-				}
-				if (user.password != password) {
-					return done(null, false, { message: 'Incorrect Password' });
-				}
-				return done(null, user);
-			} catch (error) {
-				done(error);
-			}
-		}
-	)
+    new LocalStrategy(
+        {passReqToCallback: true},
+        async (req, username, password, done) => {
+            req.session.messages = [];
+            try {
+                const user = db
+                    .prepare('SELECT * FROM users WHERE username = ?')
+                    .get(username);
+                if (!user) {
+                    return done(null, false, {message: 'Incorrect Username'});
+                }
+                if (user.password != password) {
+                    return done(null, false, {message: 'Incorrect Password'});
+                }
+                return done(null, user);
+            } catch (error) {
+                done(error);
+            }
+        }
+    )
 );
 
 passport.serializeUser(function (user, done) {
-	done(null, user.username);
+    done(null, user.username);
 });
 passport.deserializeUser(async (username, done) => {
-	try {
-		const stmt = db.prepare('SELECT * FROM users WHERE username = ?');
-		const user = stmt.get(username);
-		if (!user) {
-			done({ message: 'incorrect Username' });
-		}
-		return done(null, user);
-	} catch (error) {
-		done(error);
-	}
+    try {
+        const stmt = db.prepare('SELECT * FROM users WHERE username = ?');
+        const user = stmt.get(username);
+        if (!user) {
+            done({message: 'incorrect Username'});
+        }
+        return done(null, user);
+    } catch (error) {
+        done(error);
+    }
 });
 
 // const comparePassword = async (userPassword, requestedPassword) => {
@@ -167,5 +170,5 @@ passport.deserializeUser(async (username, done) => {
 // -------------------------------
 
 app.listen(process.env.SERVER_PORT, () =>
-	console.log(`Server Listening On Port: ${SERVER_PORT}`)
+    console.log(`Server Listening On Port: ${SERVER_PORT}`)
 );
